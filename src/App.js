d6d531fd1f5426fd56a33ab080c936e658def7fc
Jarmila Lenyiova <lenyiova.jarmila@gmail.com>
2024-06-05 14:56:44 +0200

    Finish the app

--- src/App.js
+++ src/App.js
@@ -4,31 +4,49 @@ import { useEffect, useState } from 'react';
 
 export default function App() {
   const [currencies, setCurrencies] = useState(['USD', 'EUR']);
+  const [currency1, setCurrency1] = useState('USD');
+  const [currency2, setCurrency2] = useState('EUR');
+  const [value, setValue] = useState(1);
 
   useEffect(function () {
-    async function getCurrencies() {
+    async function fetchCurrencies() {
       const response = await fetch('https://api.frankfurter.app/currencies');
       const data = await response.json();
 
       setCurrencies(Object.entries(data).map(([key, value]) => key));
     }
 
-    getCurrencies();
+    fetchCurrencies();
   }, []);
 
   return (
     <div>
-      <input type="text" />
-      <CurrencySelect currencies={currencies} />
-      <CurrencySelect currencies={currencies} />
-      <p>OUTPUT</p>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => setValue(Number(event.target.value))}
+      />
+      <CurrencySelect
+        currencies={currencies}
+        handleSetCurrency={setCurrency1}
+        currency={currency1}
+      />
+      <CurrencySelect
+        currencies={currencies}
+        handleSetCurrency={setCurrency2}
+        currency={currency2}
+      />
+      <Output currency1={currency1} currency2={currency2} value={value} />
     </div>
   );
 }
 
-function CurrencySelect({ currencies }) {
+function CurrencySelect({ currencies, handleSetCurrency, currency }) {
   return (
-    <select>
+    <select
+      value={currency}
+      onChange={(event) => handleSetCurrency(event.target.value)}
+    >
       {currencies.map((cur) => (
         <option value={cur} key={cur}>
           {cur}
@@ -36,4 +54,31 @@ function CurrencySelect({ currencies }) {
       ))}
     </select>
   );
+}
+
+function Output({ currency1, currency2, value }) {
+  const [result, setResult] = useState(1);
+
+  useEffect(
+    function () {
+      async function fetchConversion() {
+        try {
+          const response = await fetch(
+            `https://api.frankfurter.app/latest?amount=${value}&from=${currency1}&to=${currency2}`
+          );
+          const data = await response.json();
+
+          setResult(data.rates[currency2]);
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+
+      if (currency1 === currency2) return setResult(value);
+      fetchConversion();
+    },
+    [currency1, currency2, value]
+  );
+
+  return <p>{result}</p>;
 }
